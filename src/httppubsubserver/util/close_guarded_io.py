ab08c115e8762d24442787eb6ca49e707eaa9545
Timothy Moore <mtimothy984@gmail.com>
2024-11-26 14:30:02 -0800

    Fix close guarded io types for 3.9

--- src/httppubsubserver/util/close_guarded_io.py
+++ src/httppubsubserver/util/close_guarded_io.py
@@ -1,5 +1,15 @@
 import io
-from typing import Any
+from typing import Any, TYPE_CHECKING
+import sys
+
+
+if sys.version_info <= (3, 9):
+    if TYPE_CHECKING:
+        from tempfile import SpooledTemporaryFile
+
+    ChildType = Union[io.IOBase, "SpooledTemporaryFile"]
+else:
+    ChildType = io.IOBase
 
 
 def _noop(*args: Any, **kwargs: Any) -> None:
@@ -9,7 +19,7 @@ def _noop(*args: Any, **kwargs: Any) -> None:
 class CloseGuardedIO(io.IOBase):
     """Wraps an IO object, except for close() which does nothing"""
 
-    def __init__(self, child: io.IOBase) -> None:
+    def __init__(self, child: ChildType) -> None:
         self._child = child
 
     def __getattribute__(self, name: str) -> Any:
