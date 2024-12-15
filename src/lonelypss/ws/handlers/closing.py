ffdb7b4ef866325f88e651961074b3c49fa6c327
Timothy Moore <mtimothy984@gmail.com>
2024-12-15 05:34:15 -0800

    Finish refactoring websocket endpoint

---
+++ src/lonelypss/ws/handlers/closing.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+
+from lonelypss.ws.handlers.protocol import StateHandler
+from lonelypss.ws.state import State, StateClosed, StateType
+
+
+async def handle_closing(state: State) -> State:
+    """Closes the websocket normally then raises the indicated exception, if any,
+    otherwise moves to CLOSED
+    """
+
+    assert state.type == StateType.CLOSING
+    await state.websocket.close()
+    if state.exception is not None:
+        raise state.exception
+    return StateClosed(type=StateType.CLOSED)
+
+
+if TYPE_CHECKING:
+    _: StateHandler = handle_closing
--- src/lonelypss/ws/handlers/handler.py
+++ src/lonelypss/ws/handlers/handler.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING, Dict
 
 from lonelypss.ws.handlers.accepting import handle_accepting
+from lonelypss.ws.handlers.closing import handle_closing
 from lonelypss.ws.handlers.open.handler import handle_open
 from lonelypss.ws.handlers.protocol import StateHandler
 from lonelypss.ws.handlers.waiting_configure import handle_waiting_configure
@@ -10,11 +11,13 @@ _handlers: Dict[StateType, StateHandler] = {
     StateType.ACCEPTING: handle_accepting,
     StateType.WAITING_CONFIGURE: handle_waiting_configure,
     StateType.OPEN: handle_open,
+    StateType.CLOSING: handle_closing,
 }
 
 
 async def handle_any(state: State) -> State:
-    """Handle any state by delegating to the appropriate handler.
+    """Handle any state by delegating to the appropriate handler. This will raise
+    a KeyError for the CLOSED state, which does not need any further handling.
 
     Raises KeyError if no handler is found for the state type.
     """
