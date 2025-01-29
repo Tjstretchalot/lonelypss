05ba5a43fc8833dc5a58fbbd77f736eadd9b9685
Timothy Moore <mtimothy984@gmail.com>
2025-01-28 17:22:33 -0800

    Fix circular import

--- src/lonelypss/ws/handlers/open/send_simple_asap.py
+++ src/lonelypss/ws/handlers/open/send_simple_asap.py
@@ -1,6 +1,6 @@
 import asyncio
+from typing import TYPE_CHECKING
 
-from lonelypss.ws.handlers.open.senders.send_any import send_any
 from lonelypss.ws.state import (
     SimplePendingSend,
     SimplePendingSendPreFormatted,
@@ -8,12 +8,20 @@ from lonelypss.ws.state import (
     StateOpen,
 )
 
+if TYPE_CHECKING:
+    from lonelypss.ws.handlers.open.senders.send_any import send_any
+else:
+    send_any = None
+
 
 def send_asap(state: StateOpen, pending: SimplePendingSend) -> None:
     """Queues a simple message to be sent to the subscriber as soon as possible,
     where that message has already been described via one of the SimplePendingSend
     options
     """
+    global send_any
+    if send_any is None:
+        from lonelypss.ws.handlers.open.senders.send_any import send_any
 
     if state.send_task is None:
         state.send_task = asyncio.create_task(send_any(state, pending))
